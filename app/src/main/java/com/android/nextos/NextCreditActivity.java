958df893cbbb4e6fc08ecb5946531d833139fa10
darksky <darkskyofficially@gmail.com>
2024-10-04 22:26:52 +0600

    GRIFFIN

--- app/src/main/java/com/android/nextos/NextCreditActivity.java
+++ app/src/main/java/com/android/nextos/NextCreditActivity.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.android.nextos.databinding.NextcreditBinding;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -22,60 +23,80 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class NextCreditActivity extends AppCompatActivity {
-    NextcreditBinding binding;
-    String url = NextAboutActivity.SERVER_LINK+NextAboutActivity.NEXTOS_CODENAME+"/json/contributers.json";
-
-    ArrayList<UserModel> userModelArrayList = new ArrayList<>();
-    StringRequest request = new StringRequest(url.replace("main" , NextAboutActivity.NEXTOS_CODENAME), new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            try {
-                JSONObject jsonObject = new JSONObject(response);
-                JSONArray jsonArray = jsonObject.getJSONArray("credits");
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject singleObject = jsonArray.getJSONObject(i);
-                    String name = singleObject.getString("name");
-                    String photo = singleObject.getString("photo");
-                    String summery = singleObject.getString("summery");
-                    String link = singleObject.getString("link");
-                    UserModel userModel = new UserModel(name, photo, summery, link);
-                    userModelArrayList.add(userModel);
-
-                }
-                UserAdapter userAdapter = new UserAdapter(userModelArrayList, NextCreditActivity.this);
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(NextCreditActivity.this);
-                binding.nextCredit.setLayoutManager(linearLayoutManager);
-                binding.nextCredit.setAdapter(userAdapter);
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }, error -> Toast.makeText(NextCreditActivity.this, "CHECK INTERNET", Toast.LENGTH_LONG).show());
-
+    private NextcreditBinding binding;
+    private ArrayList<UserModel> userModelArrayList = new ArrayList<>();
+    private String url = NextAboutActivity.SERVER_LINK + NextAboutActivity.NEXTOS_CODENAME + "/json/contributers.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = NextcreditBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Set the title of the top bar
         TextView title = findViewById(R.id.topbar_title);
         title.setText(R.string.credit_s);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(request);
-        ImageView xBack = findViewById(R.id.back_button);
-        xBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
 
-            }
-        });
+        // Initialize the back button
+        ImageView xBack = findViewById(R.id.back_button);
+        xBack.setOnClickListener(view -> onBackPressed());
+
+        // Fetch contributors data
+        fetchContributors();
     }
 
+    private void fetchContributors() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Create a StringRequest to fetch data from the server
+        StringRequest request = new StringRequest(url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("credits");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject singleObject = jsonArray.getJSONObject(i);
+                                String name = singleObject.getString("name");
+                                String photo = singleObject.getString("photo");
+                                String summery = singleObject.getString("summery");
+                                String link = singleObject.getString("link");
+
+                                // Create a UserModel object and add it to the list
+                                UserModel userModel = new UserModel(name, photo, summery, link);
+                                userModelArrayList.add(userModel);
+                            }
+
+                            // Set up the RecyclerView with the adapter
+                            UserAdapter userAdapter = new UserAdapter(userModelArrayList, NextCreditActivity.this);
+                            LinearLayoutManager layoutManager = new LinearLayoutManager(NextCreditActivity.this);
+                            binding.nextCredit.setLayoutManager(layoutManager);
+                            binding.nextCredit.setAdapter(userAdapter);
+
+                        } catch (JSONException e) {
+                            // Handle JSON parsing errors
+                            Toast.makeText(NextCreditActivity.this, "Error parsing data", Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle network errors
+                        Toast.makeText(NextCreditActivity.this, "Failed to fetch data. Check your internet connection.", Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        // Add the request to the queue
+        queue.add(request);
+    }
+
+    @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
-
 }
